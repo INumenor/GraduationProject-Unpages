a2123b3e9ac84391c904c3297a3f209f1aa433ac
Batuh <200205011@ostimteknik.edu.tr>
2024-05-24 17:57:22 +0300

    ExplosionUpdate

--- Assets/Unpages/Scripts/BombMechanic/ExplosionTrigger.cs
+++ Assets/Unpages/Scripts/BombMechanic/ExplosionTrigger.cs
@@ -4,14 +4,25 @@ using UnityEngine;
 
 public class ExplosionTrigger : MonoBehaviour
 {
+    bool isActiv = false;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name);
-        if (other.CompareTag("TouchableItem"))
+        if (!isActiv && other.CompareTag("TouchableItem"))
         {
+            isActiv = true;
             GameService.Instance.gameControl.ObjectSpawn(other.gameObject);
             //Destroy(other.gameObject);
             //other.gameObject.SetActive(false);
+        }
+        else if (!isActiv && other.CompareTag("Item"))
+        {
+            isActiv = true;
+            Destroy(other.gameObject);
+        }
+        else if (!isActiv && other.CompareTag("Character"))
+        {
+            isActiv = true;
         }
     }
 }
