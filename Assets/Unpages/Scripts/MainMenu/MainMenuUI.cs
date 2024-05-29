fa8bf75f9738d5460b78d5e853aa1e46911266a2
ygmrbrndrhm <yagmurbernaderhem@gmail.com>
2024-05-30 01:19:05 +0300

    mixer

--- Assets/Unpages/Scripts/MainMenu/MainMenuUI.cs
+++ Assets/Unpages/Scripts/MainMenu/MainMenuUI.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    
+    public GameObject optionsCanvas;
+
+    public void OptionsButton()
+    {
+        optionsCanvas.active = true;
+    }
 }
