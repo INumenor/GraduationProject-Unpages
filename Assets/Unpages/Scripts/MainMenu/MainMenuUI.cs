80e234a194ada2d62bf91f046c7709d5336de334
Batuh <200205011@ostimteknik.edu.tr>
2024-05-30 02:53:02 +0300

    Update

--- Assets/Unpages/Scripts/MainMenu/MainMenuUI.cs
+++ Assets/Unpages/Scripts/MainMenu/MainMenuUI.cs
@@ -5,10 +5,32 @@ using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
+    public GameObject currentCanvas;
+    
     public GameObject optionsCanvas;
+    public GameObject mainMenuCanvas;
+
+    private void Start()
+    {
+        currentCanvas = mainMenuCanvas;
+    }
 
     public void OptionsButton()
     {
-        optionsCanvas.active = true;
+        currentCanvas.SetActive(false);
+        currentCanvas = optionsCanvas;
+        currentCanvas.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        currentCanvas.SetActive(false);
+        currentCanvas = mainMenuCanvas;
+        currentCanvas.SetActive(true);
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
     }
 }
