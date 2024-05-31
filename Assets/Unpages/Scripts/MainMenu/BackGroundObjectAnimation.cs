e86e47c9985f1b3dbd2f72835f95d165a579f252
ygmrbrndrhm <yagmurbernaderhem@gmail.com>
2024-05-31 11:51:06 +0300

    mainmenudesign

---
+++ Assets/Unpages/Scripts/MainMenu/BackGroundObjectAnimation.cs
@@ -0,0 +1,18 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class BackGroundObjectAnimation : MonoBehaviour
+{
+    public void Start()
+    {
+        Init();
+    }
+    public void Init()
+    {
+        //this.gameObject.transform.DOMove( + new Vector3(0, 7, 0), duration).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+    }
+}
